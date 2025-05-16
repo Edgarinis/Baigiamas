9e29b4b35912347a379f4a1cc2c1187c1b85f376
Edgaras <cedgaras112@gmail.com>
2025-05-16 15:05:09 +0300

    Snapshot violation save

--- detector.py
+++ detector.py
@@ -2,13 +2,19 @@ import cv2
 from ultralytics import YOLO
 from db import log_violation
 import time
+import datetime
+import os
 
 # Load your trained model
-model = YOLO(r'C:\Users\cedga\Desktop\Baigiamas\runs\detect\train16\weights\best.pt')
+model = YOLO(r'C:\Users\cedga\Desktop\Baigiamas\runs\detect\train17\weights\best.pt')
+
+SNAPSHOT_DIR = os.path.join('static', 'snapshots')
+os.makedirs(SNAPSHOT_DIR, exist_ok=True)
 
 VIOLATION_CLASSES = ['NO-Hardhat', 'NO-Mask', 'NO-Safety Vest']
 last_logged = {}
 COOLDOWN_SECONDS = 3
+
 
 def generate_frames():
     cap = cv2.VideoCapture(0)
@@ -29,13 +35,17 @@ def generate_frames():
                 now = time.time()
                 last_time = last_logged.get(label, 0)
                 if now - last_time > COOLDOWN_SECONDS:
-                    log_violation(label, confidence)
+                    timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+                    filename = f"{label.replace(' ', '_')}_{timestamp}.jpg"
+                    snapshot_path = os.path.join(SNAPSHOT_DIR, filename)
+                    cv2.imwrite(snapshot_path, frame)
+
+                    relative_path = os.path.relpath(snapshot_path, start='static').replace('\\', '/')
+                    log_violation(label, confidence, relative_path)
                     last_logged[label] = now
 
         _, buffer = cv2.imencode('.jpg', annotated_frame)
         frame_bytes = buffer.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
     cap.release()
